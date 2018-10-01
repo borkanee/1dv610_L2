05b444c16cddfc2c2c35485b6aeb1f2d60062a69
borkanee <grubesic.boris@gmail.com>
2018-10-01 09:24:05 +0200

    datefix

--- view/DateTimeView.php
+++ view/DateTimeView.php
@@ -8,7 +8,7 @@ class DateTimeView {
 	public function show() {
 		date_default_timezone_set('Europe/Stockholm'); 
 
-		$timeString = date('l').', the '.date('dS').' of '.date('F Y').', The time is '.date('H:i:s');
+		$timeString = date('l').', the '.date('jS').' of '.date('F Y').', The time is '.date('H:i:s');
 
 		return '<p>' . $timeString . '</p>';
 	}
